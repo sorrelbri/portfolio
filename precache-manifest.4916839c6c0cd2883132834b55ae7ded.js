cd90ed84fde1eb9360ea697615b108c24922a71f
Sorrel Bri <sorrel@SBJ-comp.local>
2019-10-17 12:30:31 -0700

    Updates

---
+++ precache-manifest.4916839c6c0cd2883132834b55ae7ded.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f485bc7ef0d2e7e0dfe18e2bef43f184",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "48985963c85917cd3a57",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "feb2bb24cf989d40935c",
+    "url": "/portfolio/static/js/2.cd22f18b.chunk.js"
+  },
+  {
+    "revision": "48985963c85917cd3a57",
+    "url": "/portfolio/static/js/main.5c457548.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
