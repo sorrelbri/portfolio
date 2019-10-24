a63af82d7cf869c5fe1c06bc6ef2ced2122bec35
Sorrel Bri <sorrel@SBJ-comp.local>
2019-10-24 15:00:06 -0700

    refactor onclick with state for AStardustTruce

--- src/components/AStardustTruce.js
+++ src/components/AStardustTruce.js
@@ -2,34 +2,45 @@ import React, { Component } from 'react';
 import AStardustTruceDetail from './AStardustTruceDetail';
 import GitHub from './logos/GitHub';
 
-const AStardustTruce = () => {
-  return (<div className="project">
-    <h3>A Stardust Truce</h3>
-      <div className="project__project-general__links">
-        <div className="project__links__github">
-          <GitHub/>
-          <a 
-            href="https://github.com/sorrelbri/a-stardust-truce"
-            >A Stardust Truce Repo
-          </a>
-        </div>
-        <div className="project__links__open-app">
-          <a href="https://astardusttruce.herokuapp.com/">Open App</a>
-        </div>
-      </div>
-    {/* <Switch>
-      <Route path="/portfolio/a-stardust-truce">
-        <AStardustTruceDetail />
-      </Route>
-      <Route path="/">
-        <Link to="/portfolio/a-stardust-truce"><h5>What is A Stardust Truce?</h5></Link>
-        <img 
-          src="%PUBLIC_URL%/a-stardust-truce-screenshot-python.png" 
-          alt="screenshot from A Stardust Truce application featuring generated Python code for a Stack instantiated as a linked list."
-        />
-      </Route>
-    </Switch> */}
-  </div>);
-}
+class AStardustTruce extends Component {
+  state = { showDetail: false }
 
+  handleClick = e => {
+    e.preventDefault();
+    this.setState(state => state.showDetail = state.showDetail
+      ? false
+      : true);
+  }
+
+  render() { 
+    return (
+      <div className="project">
+        <h3>A Stardust Truce</h3>
+        <div className="project__project-general__links">
+          <div className="project__links__github">
+            <GitHub/>
+            <a 
+              href="https://github.com/sorrelbri/a-stardust-truce"
+              >A Stardust Truce Repo
+            </a>
+          </div>
+          <div className="project__links__open-app">
+            <a href="https://astardusttruce.herokuapp.com/">Open App</a>
+          </div>
+        </div>
+        {this.state.showDetail
+          ? <AStardustTruceDetail handleClick={this.handleClick}/>
+          : <>
+            <a href="" onClick={e=>this.handleClick(e)}><h5>What is A Stardust Truce?</h5></a>
+            <img 
+              src="/a-stardust-truce-screenshot-python.png" 
+              alt="screenshot from A Stardust Truce application featuring generated Python code for a Stack instantiated as a linked list."
+            />
+          </>
+        }
+      </div>
+    );
+  }
+}
+ 
 export default AStardustTruce;
--- src/components/AStardustTruceDetail.js
+++ src/components/AStardustTruceDetail.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const AStardustTruceDetail = () => {
+const AStardustTruceDetail = (props) => {
   return (<div className="project__project-detail ">
     <p>
       A Stardust Truce seeks to elevate student comprehension of core computer science concepts by providing an interactive utility that allows users to build common data structures from prefabricated code blocks.
@@ -8,6 +8,10 @@ const AStardustTruceDetail = () => {
     <br/>
     <p>
       A Stardust Truce was built with Django to write and serve Python and JavaScript files based on user input.
+    </p>
+    <br />
+    <p>
+      <a href="" onClick={e=>props.handleClick(e)}>Back to the screenshot!</a>
     </p>
   </div>);
 }
