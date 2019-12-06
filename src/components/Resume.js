b21c0db1f2c43f1fbfd14995b2797b51331ec8e9
Sorrel Bri <sorrel.bri.june@gmail.com>
2019-12-06 12:29:38 -0800

    update resume download link with latest resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Resume = () => {
   return (<>
     <div className="Contact__link Contact__link__resume">
-      <a href="sorreljune.resume.web.1019.pdf" download>
+      <a href="sorreljune.resume.web.pdf" download>
           <p>Download Resume</p>
       </a>
     </div>
